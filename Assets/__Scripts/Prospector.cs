c056b588094bc30a96ac966f94af3f1849f68b2e
Pavel Aparcana <gpaparcana@gmail.com>
2018-11-29 16:03:08 -0500

    card processor
    stopped after writing card processor and writing on prospector

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -10,13 +10,15 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
-
+	public TextAsset deckXML;
+    public TextAsset layoutXML;
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
 
-	void Awake(){
+    void Awake(){
 		S = this;
 	}
 
@@ -25,13 +27,27 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
 
         Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference 
-        Card c;
+        //Card c;
 
-        for (int cNum = 0; cNum < deck.cards.Count; cNum++)
-        {         
-            c = deck.cards[cNum];
-            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        //for (int cNum = 0; cNum < deck.cards.Count; cNum++)
+        //{         
+        //    c = deck.cards[cNum];
+        //    c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        //}
+        layout = GetComponent<Layout>();  // Get the Layout component
+        layout.ReadLayout(layoutXML.text);
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;             
+            lCP.Add(tCP);
         }
+        return (lCP);
     }
 
 }
